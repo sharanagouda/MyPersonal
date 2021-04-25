2ab293bc9b2944129df0acb2e5906c075e50a497
Sharanagouda.k <sharanaguda.k@robosoftin.com>
2021-04-25 12:03:53 +0530

    addded a range slider

--- src/scenes/GSTCalculator/Buyer.js
+++ src/scenes/GSTCalculator/Buyer.js
@@ -17,9 +17,45 @@ import * as utils from '../../utils';
 import converter from 'number-to-words';
 
 const Buyer = () => {
-  const [TwoThousands, setTwoThousands] = useState('');
+  const [netPrice, setNetPrice] = useState(0);
+  const [gst, setGST] = useState(0);
+
+  const inputFormattedNumber = (input) => {
+    let formatter;
+    if (Platform.OS == 'ios') {
+      formatter = new Intl.NumberFormat(
+        'en-IN',
+        // {
+        //   minimumFractionDigits: 2,
+        // }
+      ).format(input);
+    } else {
+      require('intl'); // import intl object
+      require('intl/locale-data/jsonp/en-IN');
+      require('intl/locale-data/jsonp/fr-BE');
+      require('intl/locale-data/jsonp/nl-BE');
+      require('intl/locale-data/jsonp/it-IT');
+      formatter = new Intl.NumberFormat('en-IN', {
+        // style: 'currency',
+        currency: 'INR',
+      }).format(input);
+    }
+
+    return formatter;
+  };
+
+  const calculateGST = () => {
+    const gstAmount = (parseFloat(netPrice) * gst) / 100;
+    return inputFormattedNumber(gstAmount);
+  };
+  const calculateTotalPrice = () => {
+    const gstAmount = (parseFloat(netPrice) * gst) / 100;
+    const totalNetPrice = parseFloat(netPrice) + parseFloat(gstAmount);
+    return inputFormattedNumber(totalNetPrice);
+  };
+
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.maincontainer}>
       <KeyboardAwareScrollView
         scrollEnabled
         showsVerticalScrollIndicator={false}
@@ -33,85 +69,84 @@ const Buyer = () => {
           ios: utils.Scale.horizontalScale(200),
           android: utils.Scale.horizontalScale(160),
         })}>
-    <View style={styles.numberCountView}>
-          <View style={{flexDirection: 'row',     borderColor: '#000',
-    borderRadius: 5,
-    borderWidth: 2,}}>
+        <View style={styles.numberCountView}>
+          <View style={styles.inputText}>
             <TextInput
               style={styles.inputNumberText}
               underlineColorAndroid="transparent"
-              placeholder=""
+              placeholder="Net Price"
               placeholderTextColor="#9a73ef"
               autoCapitalize="none"
               editable={true}
-              value={TwoThousands}
+              value={netPrice.toString()}
               keyboardType="phone-pad"
-              selectionColor="#fff"
+              selectionColor="#000"
               textAlign={'center'}
               maxLength={7}
               onChangeText={(text) => {
-                setTwoThousands(text);
+                setNetPrice(text);
               }}
             />
-            <View style={{justifyContent: 'center', borderLeftWidth:2, borderLeftColor:'blue', paddingHorizontal:20}}>
-              <Text>Gr</Text>
+            <View style={styles.ruppeeandPercentView}>
+              <Text>Rs.</Text>
             </View>
           </View>
           <Slider
-            style={{width: 200, height: 40}}
+            style={{width: 350, height: 40}}
             minimumValue={0}
-            maximumValue={1}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#000000"
+            step={1}
+            maximumValue={2500000}
+            minimumTrackTintColor="#FF6347"
+            maximumTrackTintColor="#676767"
+            onValueChange={(value) => setNetPrice(value)}
           />
         </View>
         <View style={styles.numberCountView}>
-          <View style={{flexDirection: 'row',     borderColor: '#000',
-    borderRadius: 5,
-    borderWidth: 2,}}>
+          <View style={styles.inputText}>
             <TextInput
               style={styles.inputNumberText}
               underlineColorAndroid="transparent"
-              placeholder=""
+              placeholder="GST"
               placeholderTextColor="#9a73ef"
               autoCapitalize="none"
               editable={true}
-              value={TwoThousands}
+              value={gst.toString()}
               keyboardType="phone-pad"
-              selectionColor="#fff"
               textAlign={'center'}
-              maxLength={7}
+              selectionColor="#000"
               onChangeText={(text) => {
-                setTwoThousands(text);
+                setGST(text);
               }}
             />
-            <View style={{justifyContent: 'center', borderLeftWidth:2, borderLeftColor:'blue', paddingHorizontal:20}}>
-              <Text>Gr</Text>
+            <View style={styles.ruppeeandPercentView}>
+              <Text>%</Text>
             </View>
           </View>
           <Slider
-            style={{width: 200, height: 40}}
+            style={{width: 350, height: 40}}
             minimumValue={0}
-            maximumValue={1}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#000000"
+            step={1}
+            maximumValue={50}
+            minimumTrackTintColor="#FF6347"
+            maximumTrackTintColor="#676767"
+            onValueChange={(value) => setGST(value)}
           />
         </View>
         <Card style={{padding: 10, margin: 10, backgroundColor: '#25e4a2'}}>
           <Text>Gross price</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Rs. {inputFormattedNumber(netPrice)}</Text>
         </Card>
         <Card style={{padding: 10, margin: 10, backgroundColor: '#498dfc'}}>
           <Text>CGST Amount</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Rs. {calculateGST()}</Text>
         </Card>
         <Card style={{padding: 10, margin: 10, backgroundColor: '#aaaaaa'}}>
           <Text>IGST Amout</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Rs. {calculateGST()}</Text>
         </Card>
         <Card style={{padding: 10, margin: 10, backgroundColor: '#434343'}}>
           <Text>Total Tax</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Rs. {calculateTotalPrice()}</Text>
         </Card>
       </KeyboardAwareScrollView>
     </View>
@@ -120,10 +155,26 @@ const Buyer = () => {
 export default Buyer;
 
 const styles = StyleSheet.create({
+  maincontainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     marginTop: 40,
     backgroundColor: '#fff',
+  },
+  ruppeeandPercentView: {
+    justifyContent: 'center',
+    borderLeftWidth: 2,
+    borderLeftColor: 'blue',
+    paddingHorizontal: 20,
+  },
+  inputText: {
+    flexDirection: 'row',
+    borderColor: '#000',
+    borderRadius: 5,
+    borderWidth: 2,
+    paddingLeft: 20,
   },
   inputNumberText: {
     height: 40,
